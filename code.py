e6f26df6c4f066df272d2e8a8988f62da74eaef3
Danny <dannylee94049@gmail.com>
2020-11-02 22:28:56 +0800

    update tf from numpy array to dict
    can fast for 5s (20s -> 15s)

--- code.py
+++ code.py
@@ -3,7 +3,7 @@ from collections import Counter
 from tqdm import tqdm
 from datetime import datetime,timezone,timedelta
 from functools import reduce
-from argparse import ArgumentParser
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
 def timestamp():
     dt1 = datetime.utcnow().replace(tzinfo=timezone.utc)
@@ -20,11 +20,11 @@ def file_iter(_type):
             with open(doc_path+name+'.txt') as f:
                 yield f.readline()
 
-parser = ArgumentParser()
-parser.add_argument("-B", default=0.75, type=float)
-parser.add_argument("-K1", default=3.5, type=float)
-parser.add_argument("-K3", default=1000, type=float)
-parser.add_argument("-use_q_tf", default='F', type=str, choices=['T','F'], help="will deactivate K3")
+parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+parser.add_argument("-B", default=0.75, type=float ,help=" ")
+parser.add_argument("-K1", default=3.5, type=float ,help=" ")
+parser.add_argument("-K3", default=1000, type=float ,help=" ")
+parser.add_argument("-use_q_tf", default='F', type=str, choices=['T','F'], help="whether to use query's tf; will deactivate K3")
 args = parser.parse_args()
 str2bool = {'T':True, 'F':False}
 args.use_q_tf = str2bool[args.use_q_tf]
@@ -65,37 +65,27 @@ for txt in tqdm(file_iter("d")):
 voc = reduce(set.union, map(set, map(dict.keys, list_q_tf)))
 voc = list(voc)
 
-# tf to array
+# df
 doc_len = []
-np_d_tf = np.zeros([len(d_list), len(voc)])
+np_df = dict.fromkeys(voc, 0)
 for i, doc in tqdm(enumerate(doc_list)):
     doc_len += [len(doc)]
-    for j, w in enumerate(voc):
-        np_d_tf[i][j] += list_d_tf[i][w]
+    for w in voc:   
+        np_df[w] += 1 if list_d_tf[i][w] > 0 else 0
 doc_avg_len = np.array(doc_len).mean()
-
-if args.use_q_tf:
-    np_q_tf = np.zeros([len(q_list), len(voc)])
-    for i, doc in tqdm(enumerate(query_list)):
-        for j, w in enumerate(voc):
-            np_q_tf[i][j] += list_q_tf[i][w]
-
-# df
-np_df = np.count_nonzero(np_d_tf, axis=0) #n_i
 
 # sim_array
 sim_array = np.zeros([len(q_list), len(d_list)])
 for q in tqdm(range(len(q_list))):
     for d in range(len(d_list)):
         for w in query_list[q].split():
-            i = voc.index(w)
-            d_tf = (K1+1)*np_d_tf[d][i] / (K1*((1-B) + B*doc_len[d]/doc_avg_len) + np_d_tf[d][i])
+            d_tf = (K1+1)*list_d_tf[d][w] / (K1*((1-B) + B*doc_len[d]/doc_avg_len) + list_d_tf[d][w])
             if args.use_q_tf:
-                q_tf = (K3+1)*np_q_tf[q][i] / (K3+np_q_tf[q][i])
+                q_tf = (K3+1)*list_q_tf[q][w] / (K3+list_q_tf[q][w])
             else:
                 q_tf = 1
-            idf = np.log(1+(len(d_list)-np_df[i]+0.5) / (np_df[i]+0.5))
-            # idf = np.power(idf, 2)
+            idf = np.log(1+(len(d_list)-np_df[w]+0.5) / (np_df[w]+0.5))
+            idf = np.power(idf, 2)
             sim_array[q][d] += d_tf * q_tf * idf
 
 # output
